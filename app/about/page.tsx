ebf516a93721c4b8371b423deddfe81aeba59fde
yousuf hussain <hussainsyed0601@gmail.com>
2025-08-07 11:53:46 +0530

    heading changes a bit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className=''>page
+
+        this is th about page 
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing woooow{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
+              blah blah blah
             </code>
             .
           </li>
@@ -39,7 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now really 
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs mewooow 
           </a>
         </div>
       </main>
